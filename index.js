e4c5ad126b2909c920e3fba7dc508c35eeade327
madelinedulchin <madeline.dulchin@gmail.com>
2022-09-30 14:02:04 -0400

    commit tree 1635ea3117af312da92c6808776648a1b0e19ab6

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+ 
+//   let findRecord = record.find(superbowlWin)
+
+// function superbowlWin(record) {
+//     if (record.result === "W" ){
+//        return record.year
+// }
+
+// }
+
+function superbowlWin(record) {
+    let win = record.find((arr) => arr.result === "W" )
+    if(win){
+        return win.year
+    }
+}
+
+
+
+
+// function suberbowlWin(record) {
+//     let winner = record.find((game) => game.result === "W" )
+//     if (winner) {
+//         return winner.year
+//     }
+// }
